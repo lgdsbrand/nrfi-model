dfd376b78858d16d3f9debc3891da1559907c7df
lgdsbrand <legendsbrandcompany@gmail.com>
2025-08-03 18:17:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,16 +3,27 @@ from update_models import get_today_games, calculate_nrfi
 
 st.set_page_config(page_title="NRFI/YRFI Model", layout="wide")
 
-# Top buttons
-col1, col2 = st.columns([4,1])
+# Top layout
+col1, col2 = st.columns([4, 1])
 with col1:
     st.title("⚾ NRFI / YRFI Model")
 with col2:
     st.markdown("[🏠 Back to Homepage](https://lgdsbrand.streamlit.app)")
 
+# Get data
 games_df = get_today_games()
 final_df = calculate_nrfi(games_df)
 
-# Hide index, format Confidence
+# Add Confidence formatting
 final_df["Confidence %"] = final_df["Confidence %"].astype(str) + "%"
-st.dataframe(final_df.style.hide(axis="index"), use_container_width=True)
+
+# Color styling
+def highlight_prediction(val):
+    if val == "NRFI":
+        return "background-color: lightgreen; color: black;"
+    else:
+        return "background-color: lightcoral; color: black;"
+
+styled_df = final_df.style.applymap(highlight_prediction, subset=["Prediction"]).hide(axis="index")
+
+st.dataframe(styled_df, use_container_width=True)
